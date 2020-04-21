796940ae007aaf1845fdf758ffe04f04e1df737f
Henrique Ritter <henrique-ritter@hotmail.com>
2020-04-21 15:19:28 -0300

    finished challenge

--- src/app.js
+++ src/app.js
@@ -2,33 +2,94 @@ const express = require("express");
 const cors = require("cors");
 //lsof -i tcp:3333 //verifica servico na porta 3333 
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+//Function validate UUID
+function validateRepoId(request, response, next){
+  const { id } = request.params;
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid ID Format'});
+  }
+  return next();
+}
 
+app.use('/repositories/:id', validateRepoId);
+
+const repositories = []; 
+
+//List repositories
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
+//Create repositories
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository = {
+    id: uuid(),
+    title,url,techs,
+    likes:0
+  }
+  repositories.push(repository);
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repoIndex = repositories.findIndex(repository => repository.id === id);
+  if (repoIndex < 0){
+    return response.status(400).json({ error: 'Repo ID doenst exist'});
+  }
+  const likes = repositories[repoIndex].likes;
+  repository = {
+    id, 
+    title, 
+    url, 
+    techs,
+    likes
+  }
+  repositories[repoIndex] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  repoIndex = repositories.findIndex(repository => repository.id === id);
+  if (repoIndex < 0){
+    return response.status(400).json({ error: 'Cannot Delete, Repo ID not found '});
+  }
+
+  repositories.splice(repoIndex, 1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  repoIndex = repositories.findIndex(repository => repository.id === id);
+  if (repoIndex < 0){
+    return response.status(400).json({ error: 'Repo ID doenst exist'});
+  }
+  
+  //Add like to repository
+  const { title, url, techs } = repositories[repoIndex];
+  const likes = repositories[repoIndex].likes+1;
+
+  repository = {
+    id, title, url, techs, likes
+  }
+
+  repositories[repoIndex] = repository;
+  return response.json(repository);
 });
 
 module.exports = app;
